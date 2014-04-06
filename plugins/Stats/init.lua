5bab347bede24e6601fe97d2fcee77da700945a2
ShadowNinja <shadowninja@minetest.net>
2014-04-06 18:42:19 -0400

    Add Stats plugin

---
+++ plugins/Stats/init.lua
@@ -0,0 +1,127 @@
+--[[
+-- Plugin to generate network statistics
+--
+-- This requires the Oper plugin, a valid operator block on the server, and
+--  the oper:spy privilege.
+--]]
+
+local json = require("json")
+
+local waitingListEnd = false
+local chanUsers = {}
+local chansWaiting = {}
+
+
+bot:registerCommand("genstats", {
+	desctiption = "Generate network statistics",
+	privs = {stats=true},
+	action = function(conn, msg, args)
+		waitingListEnd = true
+		conn:queue(irc.Message("LIST"))
+		return "Generating statistics...", true
+	end,
+})
+
+
+local function generateStats(conn)
+	local nodes = {}
+	local edges = {}
+	local nickIDs = {}
+
+	local id = 1
+	for channel, nicks in pairs(chanUsers) do
+		local cid = id
+		table.insert(nodes, {
+			id = id,
+			label = channel,
+			fontSize = 24,
+			color = "green",
+		})
+		id = id + 1
+		for i, nick in pairs(nicks) do
+			if nickIDs[nick] then
+				table.insert(edges, {
+					from = cid,
+					to = nickIDs[nick],
+				})
+			else
+				nickIDs[nick] = id
+				table.insert(nodes, {
+					id = id,
+					label = nick,
+					shape = "box",
+				})
+				table.insert(edges, {
+					from = cid,
+					to = id,
+				})
+				id = id + 1
+			end
+		end
+	end
+
+	-- Reset tables
+	chanUsers = {}
+	chansWaiting = {}
+
+	-- Write data
+	local f, err = io.open("map.js", "w")
+	if not f then return end
+	f:write("window.nodes = ")
+	f:write(json.encode(nodes))
+	f:write("\n")
+	f:write("window.edges = ")
+	f:write(json.encode(edges))
+	f:write("\n")
+	f:close()
+end
+
+
+-- LIST
+bot:hook("Do322", function(conn, msg)
+	if not waitingListEnd then
+		return
+	end
+	local channel = msg.args[2]
+	chanUsers[channel] = chanUsers[channel] or {}
+	chansWaiting[channel] = true
+	conn:queue(irc.Message("WHO", {"!"..channel}))
+end)
+
+
+-- End of /LIST
+bot:hook("Do323", function(conn, msg)
+	if waitingListEnd then
+		waitingListEnd = false
+	end
+end)
+
+
+-- WHO list
+bot:hook("Do352", function(conn, msg)
+	local channel = msg.args[2]
+	local nick = msg.args[6]
+	if not chansWaiting[channel] then
+		return
+	end
+	if not nick:find("Serv$") then
+		table.insert(chanUsers[channel], nick)
+	end
+end)
+
+
+-- End of /WHO list
+bot:hook("Do315", function(conn, msg)
+	local channel = msg.args[2]
+	if not chansWaiting[channel] then
+		return
+	end
+	chansWaiting[channel] = nil
+
+	-- Return if we are still waiting for more channels
+	for _, _ in pairs(chansWaiting) do
+		return
+	end
+	generateStats(conn)
+end)
+
