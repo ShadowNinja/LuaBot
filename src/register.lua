703e7206fdec0d608704410c25f36711ff7f1f44
ShadowNinja <shadowninja@minetest.net>
2014-05-22 19:47:07 -0400

    Make registered functions unloadable

--- src/register.lua
+++ src/register.lua
@@ -1,19 +1,30 @@
 
-bot.registeredFuncs = {}
+local registeredFuncs = {}
 
 
+-- For internal use only
 function bot:register(class, func)
-	self.registeredFuncs[class] = self.registeredFuncs[class] or {}
-	table.insert(self.registeredFuncs[class], func)
+	registeredFuncs[class] = registeredFuncs[class] or {}
+	table.insert(registeredFuncs[class], func)
 end
 
 
 function bot:call(class, ...)
-	if not self.registeredFuncs[class] then
+	if not registeredFuncs[class] then
 		return
 	end
-	for _, func in ipairs(self.registeredFuncs[class]) do
-		func(...)
+	-- Call internal functions first
+	if registeredFuncs[class] then
+		for _, func in ipairs(registeredFuncs[class]) do
+			func(...)
+		end
+	end
+	-- Then call registered functions in plugins
+	for pluginName, plugin in pairs(self.plugins) do
+		local registered = plugin.on
+		if registered and registered[class] then
+			registered[class](...)
+		end
 	end
 end
 
