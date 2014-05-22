62bd4b80306fffec1856836e580b2ae01254911c
ShadowNinja <shadowninja@minetest.net>
2014-05-21 20:21:21 -0400

    Add repeating events and sub-second precision to schedule

--- src/schedule.lua
+++ src/schedule.lua
@@ -1,28 +1,40 @@
 
-bot.schedule = {
-	events = {},
-}
+local socket = require("socket")  -- For gettime
 
 
-function bot.schedule:add(after, func, ...)
-	local time = os.time()
-	table.insert(self.events, {
-		time = time + after,
+bot.scheduledEvents = {}
+
+
+function bot:schedule(after, repeats, func, ...)
+	assert(type(after) == "number" and after >= 0,
+			"Argument #1 to schedule must be a positive number")
+	assert(type(func) == "function",
+			"Argument #3 to schedule must be a function")
+	local time = socket.gettime()
+	table.insert(self.scheduledEvents, {
+		time = time,
+		after = after,
 		func = func,
-		args = {...}
+		repeats = repeats,
+		args = {...},
 	})
 end
 
 
 bot:register("step", function(dtime)
-	local time = os.time()
-	local events = bot.schedule.events
+	local time = socket.gettime()
+	local events = bot.scheduledEvents
 	local i = 1
 	while events[i] ~= nil do
 		local event = events[i]
-		if event.time <= time then
-			event.func(unpack(event.args))
-			table.remove(events, i)
+		if (event.time + event.after) <= time then
+			event.func(table.unpack(event.args))
+			if event.repeats then
+				event.time = time
+				i = i + 1
+			else
+				table.remove(events, i)
+			end
 		else
 			i = i + 1
 		end
