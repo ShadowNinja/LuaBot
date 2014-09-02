bdb3995493b2b377e547afd301a5cf191e7047bd
ShadowNinja <shadowninja@minetest.net>
2014-09-02 14:40:18 -0400

    Add support for references and recursion to LuaDB

--- src/db.lua
+++ src/db.lua
@@ -1,4 +1,5 @@
 local pretty = require("pl.pretty")
+local serialization = require("src.serialize")
 
 --- Loads a Lua value from a file created with saveLuaData().
 -- The lua file may contain a nil.  If that is valid you will have
@@ -7,12 +8,18 @@ local pretty = require("pl.pretty")
 -- @return The loaded data, or nil on error.
 -- @return Error message, or nil on success.
 -- @see saveLuaData
-local function loadLuaData(filename)
+local function loadLuaData(filename, advanced)
 	local file, err = io.open(filename, "r")
 	if not file then
 		return nil, err
 	end
-	local data, err = pretty.read(file:read("*all"))
+	local str = file:read("*all")
+	local data, err
+	if advanced then
+		data, err = serialization.deserialize(str)
+	else
+		data, err = pretty.read(str)
+	end
 	file:close()
 	return data, err
 end
@@ -23,15 +30,16 @@ end
 -- @param indent Indentation to use, if any.
 -- @return Error message, or nil on success.
 -- @see loadLuaData
-local function saveLuaData(filename, data, indent)
-	local dataStr, err = pretty.write(data, indent or "")
-	if not dataStr then
-		return err
+local function saveLuaData(filename, data, advanced, indent)
+	local dataStr, err
+	if advanced then
+		dataStr, err = serialization.serialize(data)
+	else
+		dataStr, err = pretty.write(data, indent or "")
 	end
+	if err then return err end
 	local file, err = io.open(filename, "w")
-	if not file then
-		return err
-	end
+	if not file then return err end
 	file:write(dataStr)
 	file:close()
 end
@@ -46,32 +54,35 @@ meta.__index = meta
 
 --- Database class that stores data in a file in Lua-like format.
 -- @param filename The file for the database to operate on.
+-- @param advanced Whether the file is saved in a advanced format with support
+--		for things like multiple references, recursion, and functions.
 -- @param data Initial data for the database.
 -- @return A database instance.
--- @field data Database data.
 -- @field filename Database filename.
-function LuaDatabase(filename, data)
+-- @field data Database data.
+function LuaDatabase(filename, advanced, data)
 	local o = {
 		filename = filename,
+		advanced = advanced,
 		data = data or {},
 	}
 	return setmetatable(o, meta)
 end
 
---- Save data to the database file.
--- @name LuaDatabase:save
+--- Load data from the database file.
+-- @name LuaDatabase:load
 function meta:load()
-	local data, err = loadLuaData(self.filename)
+	local data, err = loadLuaData(self.filename, self.advanced)
 	if not err then
 		self.data = data
 	end
 	return err
 end
 
---- Load data from the database file.
--- @name LuaDatabase:load
+--- Save data to the database file.
+-- @name LuaDatabase:save
 -- @param indent Indentation to use, if any.
 function meta:save(indent)
-	return saveLuaData(self.filename, self.data, indent)
+	return saveLuaData(self.filename, self.data, self.advanced, indent)
 end
 
